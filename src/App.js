4aa6f09128770aef49f944d6436235c3c460fb92
Vinicius Andre Ribeiro Marinho <viniciusandrem22@gmail.com>
2022-05-08 12:07:16 -0300

    iniciei a home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import camiseta1 from "./imagens/camiseta1.jpg"
+import camiseta2 from "./imagens/camiseta2.jpg"
+import camiseta3 from "./imagens/camiseta3.jpg"
+import camiseta4 from "./imagens/camiseta4.jpg"
+import camiseta5 from "./imagens/camiseta5.jpg"
+import camiseta6 from "./imagens/camiseta6.jpg"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className='containerCamisas'>
+        <div className='cardCamisas'>
+          <img src={camiseta1} className="tamanhoImagem" alt="camiseta1" />
+          <p>Camiseta 1</p>
+          <p>R$ 89,99</p>
+          <button>Adicionar ao carrinho</button>
+        </div>
+        <div className='cardCamisas'>
+          <img src={camiseta2} className="tamanhoImagem" alt="camiseta2" />
+          <p>Camiseta 2</p>
+          <p>R$ 69,99</p>
+          <button>Adicionar ao carrinho</button>
+        </div>
+        <div className='cardCamisas'>
+          <img src={camiseta3} className="tamanhoImagem" alt="camiseta3" />
+          <p>Camiseta 3</p>
+          <p>R$ 49,99</p>
+          <button>Adicionar ao carrinho</button>
+        </div>
+        <div className='cardCamisas'>
+          <img src={camiseta4} className="tamanhoImagem" alt="camiseta4" />
+          <p>Camiseta 4</p>
+          <p>R$ 39,99</p>
+          <button>Adicionar ao carrinho</button>
+        </div>
+       <div className='cardCamisas'>
+          <img src={camiseta5} className="tamanhoImagem" alt="camiseta5" />
+          <p>Camiseta 5</p>
+          <p>R$ 59,99</p>
+          <button>Adicionar ao carrinho</button>
+        </div>
+        <div className='cardCamisas'>
+          <img src={camiseta6} className="tamanhoImagem" alt="camiseta6" />
+          <p>Camiseta 6</p>
+          <p>R$ 109,99</p>
+          <button>Adicionar ao carrinho</button>
+        </div>
+      </div>
     </div>
   );
 }
