7117dfce7249409bb9d8524298bcd2b4961c53f1
Walmir-Junior <juniorsouzafig@gmail.com>
2022-05-12 17:44:24 -0300

    iniciei os filtros no componente produtos

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,13 +3,14 @@ import Logo from '../../imagens/Logo.png'
 import { Base, Centro, Direita, Esquerda } from "./styled"
 import { ShoppingCartOutlined } from '@material-ui/icons'
 
+
 class Header extends React.Component{
 
     render(){
         return(
             <Base>
                 <Esquerda>
-                    left
+                    <h1>Astro, o melhor de dois mundos</h1>
                 </Esquerda>
                 <Centro>
                     <img src={Logo} alt="logo"/>
